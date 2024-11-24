800312a529e893dde6a90b1c8c5d4aef9cb5007c
Paul Wintz <19618780+pwintz@users.noreply.github.com>
2024-11-24 03:56:14 -0800

    Update README to describe how to run simulations using scripts in repeatability_evaluation directory for REP reviewer.

--- repeatability_evaluation/run_example_in_container.sh
+++ repeatability_evaluation/run_example_in_container.sh
@@ -1,14 +1,21 @@
 #! /usr/bin/bash
 
+# Make the script fail if any commands fail.
+set -Eeuo pipefail
+
 EXAMPLE_NAME=$1
 CONFIG_FILE=$2
 
-mkdir $(pwd)/${EXAMPLE_NAME}_experiments
+# Make the experiments folder in the host environment if it does not already exist.
+mkdir  -p $(pwd)/${EXAMPLE_NAME}_experiments
 echo "Experiment results will be available on the host machine in $(pwd)/${EXAMPLE_NAME}_experiments/"
 
+# Run the experiment in docker, including generating plots.
 docker run --rm \
   -v "$(pwd)/${EXAMPLE_NAME}_experiments:/home/dcuser/examples/${EXAMPLE_NAME}/experiments" \
   sharc:latest \
-  bash -c "cd /home/dcuser/examples/${EXAMPLE_NAME} && sharc --config_filename ${CONFIG_FILE}" 
+  bash -c "cd /home/dcuser/examples/${EXAMPLE_NAME} \
+        && sharc --config_filename ${CONFIG_FILE} \
+        && ./generate_example_figures.py" 
 
 echo "Experiments finished. See results in $(pwd)/${EXAMPLE_NAME}_experiments."
