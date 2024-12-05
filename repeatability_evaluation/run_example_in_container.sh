2abeb7e0b795f56b4a7367081543db54dc875b51
Paul Wintz <19618780+pwintz@users.noreply.github.com>
2024-12-05 02:35:41 -0800

    Make examples interactive after they run.

--- repeatability_evaluation/run_example_in_container.sh
+++ repeatability_evaluation/run_example_in_container.sh
@@ -10,16 +10,30 @@ CONFIG_FILE=$2
 mkdir  -p $(pwd)/${EXAMPLE_NAME}_experiments
 echo "Experiment results will be available on the host machine in $(pwd)/${EXAMPLE_NAME}_experiments/"
 
-# Run the experiment in docker, including generating plots.
+# # Run the experiment in docker, including generating plots.
+# # Explanation of arguments:
+#   #         --rm: Remove the container when it exits, so we don't fill up the hard drive. \
+#   # --privileged: Run the container with elevated permissions so that Scarab can use "setarch x86_64 -R". 
+#   #           -v: Mount a volume so that the results of experiments persist on the host machine.
+# docker run --rm --privileged \
+#   -v "$(pwd)/${EXAMPLE_NAME}_experiments:/home/dcuser/examples/${EXAMPLE_NAME}/experiments" \
+#   sharc:latest \
+#   bash -c "cd /home/dcuser/examples/${EXAMPLE_NAME} \
+#         && sharc --config_filename ${CONFIG_FILE} --failfast \
+#         && ./generate_example_figures.py" 
+
+# Run the experiment INTERACTIVELY in docker, including generating plots, allowing you to examine the container when the experiment finishes.
 # Explanation of arguments:
-  #         --rm: Remove the container when it exits, so we don't fill up the hard drive. \
+  #         --rm: Remove the container when it exits, so we don't fill up the hard drive.
+  #          -it: Interactive
   # --privileged: Run the container with elevated permissions so that Scarab can use "setarch x86_64 -R". 
   #           -v: Mount a volume so that the results of experiments persist on the host machine.
 docker run --rm --privileged \
+  -it \
   -v "$(pwd)/${EXAMPLE_NAME}_experiments:/home/dcuser/examples/${EXAMPLE_NAME}/experiments" \
   sharc:latest \
   bash -c "cd /home/dcuser/examples/${EXAMPLE_NAME} \
         && sharc --config_filename ${CONFIG_FILE} --failfast \
-        && ./generate_example_figures.py" 
+        && ./generate_example_figures.py && bash" 
 
 echo "Experiments finished. See results in $(pwd)/${EXAMPLE_NAME}_experiments."
