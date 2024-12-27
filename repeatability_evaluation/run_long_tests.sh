82e6d1b82680027b119949f4c48df17980984d97
Paul Wintz <19618780+pwintz@users.noreply.github.com>
2024-12-26 17:21:30 -0800

    Update README and long tests to improve documentation and test coverage.

--- repeatability_evaluation/run_long_tests.sh
+++ repeatability_evaluation/run_long_tests.sh
@@ -5,8 +5,12 @@
 # Make the script fail if any commands fail.
 set -Eeuo pipefail
 
+# Run unit tests.
 docker run --rm sharc:latest bash -c "cd /home/dcuser/resources/tests && ./run_all.sh"
 
 docker run --privileged --rm sharc:latest bash -c "cd /home/dcuser/examples/acc_example && sharc --config_file smoke_test.json"
+
+# Run the acc example using fake delays. This include testing the figure generation script, so it is slightly different that running the smoke test.
+./run_acc_example_with_fake_delays.sh
 
 echo "Tests succeeded."
