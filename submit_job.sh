77fe024a2095ea2b7e47b547175f2fe5d1b518e7
YasinSonmez <sonmezyasin6006@gmail.com>
2025-10-04 13:54:15 -0700

    organzie the docs

--- submit_job.sh
+++ submit_job.sh
@@ -39,7 +39,7 @@ fi
 IMAGE="sharc_latest.sif"
 
 # Example to run (acc_example or cartpole)
-EXAMPLE="cartpole"
+EXAMPLE="acc_example"
 
 # Configuration files array (edit as needed)
 CONFIG_FILES=(
